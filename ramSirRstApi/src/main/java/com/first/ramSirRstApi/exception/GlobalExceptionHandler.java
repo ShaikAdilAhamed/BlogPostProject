b0948ac1401a9976ed76b8450cf8b049aaa46d6e
Shaik Adil Ahamed <s.adilahmed786@gmail.com>
2025-01-25 12:09:15 +0530

    @validation method over ride and get the need mssgs

--- ramSirRstApi/src/main/java/com/first/ramSirRstApi/exception/GlobalExceptionHandler.java
+++ ramSirRstApi/src/main/java/com/first/ramSirRstApi/exception/GlobalExceptionHandler.java
@@ -1,15 +1,22 @@
 package com.first.ramSirRstApi.exception;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(exception = ResourceNotFoundException.class)
 	public ResponseEntity< CustomExceptionResponse>resourceNotFoundException(ResourceNotFoundException me,WebRequest we) {
@@ -40,5 +47,22 @@ public class GlobalExceptionHandler {
 		errorDetails.setDetails(we.getDescription(false));
 		errorDetails.setMessage(exception.getMessage());
 		return new ResponseEntity<>(errorDetails,HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	//overriding for VAlidation annotation and creating custom exception and setting here VAlidation mssgs set and print
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		Map<String,String>errors= new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error)-> {
+			String fieldName=((FieldError)error).getField();
+			String message=error.getDefaultMessage();
+			errors.put(fieldName, message);
+			
+		});
+		
+		return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
 	}
 }
