7739bff8f59cb7ab73f5e2f123e91e0d35096042
ShaikAdilAhamed <89061108+ShaikAdilAhamed@users.noreply.github.com>
2025-02-19 17:23:03 +0530

    Update BlogPostController.java
    asdfg

--- ramSirRstApi/src/main/java/com/first/ramSirRstApi/controller/BlogPostController.java
+++ ramSirRstApi/src/main/java/com/first/ramSirRstApi/controller/BlogPostController.java
@@ -34,9 +34,6 @@ public class BlogPostController {
 	public ResponseEntity<BlogPostDTO> findBlog(@PathVariable("postId") Integer id) {
 
 System.out.println("--------------i had made changes from git website----------------");
-System.out.println("--------------i had made changes from git website copy----------------");
-
-System.out.println("--------------i had made changes from git website edited----------------");
 
 		BlogPostDTO getById=blogPostService.findByBlogPostId(id);
 		return new ResponseEntity(getById, HttpStatus.OK);
